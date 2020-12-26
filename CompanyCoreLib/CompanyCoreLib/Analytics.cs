789b314a882ee70eaf99d741dd749773e08440fd
m0l0des <64148066+m0l0des@users.noreply.github.com>
2020-12-26 09:48:14 +0300

    Add files via upload

---
+++ CompanyCoreLib/CompanyCoreLib/Analytics.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyCoreLib
+{
+    public class Analytics
+    {
+        public List<DateTime> PopularMonths(List<DateTime> dates) {
+    var DateTimeWithCounterList = new List<Tuple<DateTime, int>>();
+
+    int PreviousYear = DateTime.Now.Year - 1;
+    foreach (DateTime IterDate in dates)
+    {
+        if (IterDate.Year == PreviousYear)
+        {
+            var DateMonthStart = new DateTime(IterDate.Year, IterDate.Month, 1, 0, 0, 0);
+
+            var index = DateTimeWithCounterList.FindIndex(item => item.Item1 == DateMonthStart);
+
+            if (index == -1)
+            {
+                DateTimeWithCounterList.
+                    Add( new Tuple<DateTime,int>(DateMonthStart, 1) );
+            }
+            else
+            {
+                DateTimeWithCounterList[index] = Tuple.Create(DateTimeWithCounterList[index].Item1, DateTimeWithCounterList[index].Item2 + 1);
+            }
+        }
+    }
+
+    return DateTimeWithCounterList
+        .OrderByDescending(item => item.Item2)
+        .ThenBy(item => item.Item1)
+        .Select(item => item.Item1)
+        .ToList();
+}
+    }
+}
